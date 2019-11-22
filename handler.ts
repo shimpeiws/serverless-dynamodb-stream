78182682cddb8ba7841696c2fe0d79b28726a380
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-11-22 17:01:53 +0900

    Implement dynamo triggered function

--- handler.ts
+++ handler.ts
@@ -1,12 +1,36 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import {
+  Context,
+  Handler,
+  APIGatewayEvent,
+  Callback,
+  DynamoDBStreamEvent
+} from "aws-lambda";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: Handler = async (
+  event: APIGatewayEvent,
+  _context: Context,
+  _callback: Callback
+) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message:
         "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event
+    })
+  };
+};
+
+export const dynamoTriggered: Handler = async (
+  event: DynamoDBStreamEvent,
+  _context: Context,
+  _callback: Callback
+) => {
+  console.info("event.Records", event.Records);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: event.Records
     })
   };
 };
